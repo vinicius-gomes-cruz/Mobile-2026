7cc255b357ba53e1a9857d56873a7401ad410504
Vinicius <niciugcruz@gmail.com>
2026-03-26 20:27:54 -0300

    teste

--- Mobile-2026/meuApp/App.tsx
+++ Mobile-2026/meuApp/App.tsx
@@ -1,8 +1,47 @@
-import React from "react";
-import MeuComponente from "./src/components/MeuComponente";
+import React, {Component} from "react";
+import { View, Text, Image } from "react-native";
 
-function App() {
-  return <MeuComponente/>
+interface Imagemprops{
+  largura: number;
+  altura: number;
+  nome2 : string;
 }
 
+class App extends Component {
+      render() {
+         let nome = 'IFPR';
+         return (
+             <View>
+                 <Text>Olá Mundo!</Text>
+                 <Text>Dispositivos Móveis!</Text>
+                 
+                 <Text style={{color: 'red', fontSize: 25, margin: 15}}>
+                     Meu primeiro App!
+                 </Text>
+ 
+                 <Imagem largura={300} altura={300} nome2={"Teste Props Logo React"}/>
+ 
+                 <Text style={{fontSize: 30 }}>{nome}</Text>
+ 
+             </View>
+         );
+     }
+ }
+ 
+ class Imagem extends Component<Imagemprops> {
+     render() {
+         let img = 'https://reactnative.dev/img/tiny_logo.png';
+ 
+         return(
+           <View>
+             <Image
+              source={{uri: img}}
+              style={{width: this.props.largura, height: this.props.altura}}
+             />
+             <Text>{this.props.nome2}</Text>
+         </View>
+         )
+     }
+ }
+ 
 export default App;
